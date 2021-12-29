d6d8dde09f7aa0a1f8ec5434b3dd5d60d2f899d6
Tam Nguyen <tam.nguyen.10119@gmail.com>
2021-12-29 22:32:22 +0700

    Adding prequisites files

--- controllers/createNewBook.js
+++ controllers/createNewBook.js
@@ -1,3 +1,10 @@
 const Redis = require('redis')
 
 const redisClient = Redis.createClient()
+
+
+const createNewBook = (req,res) => {
+    const book = req.body
+    redisClient.set('book', book)
+    
+}
