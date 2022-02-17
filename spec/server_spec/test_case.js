dd7f957c62c8c5e8e8748c41e56f14d6d7ad54c1
Tam Nguyen <tam.nguyen.10119@gmail.com>
2022-02-17 23:04:06 +0700

    Adding test cases

--- spec/server_spec/test_case.js
+++ spec/server_spec/test_case.js
@@ -106,11 +106,15 @@ describe("Route ", () => {
       next();
     })
     getBookCacheAdminStub = sinon.stub(books, 'getBookCacheAdmin').callsFake(function(req,res,next) {
-      if(req.session.role=='admin') console.log('admin')
-
+      if(req.session.admin) res.send('admin')
+      else next()
     })
     getBookCacheUserStub = sinon.stub(books, 'getBookCacheUser').callsFake(function(req,res,next) {
-      if(req.session.role=='user') console.log('user')
+      if(req.session.user) res.send('user')
+      else next()
+    })
+    unidentifiedUser = sinon.stub(books, 'unidentifiedUser').callsFake(function(req,res,next) {
+      res.send('unidentified')
     })
     server = require('../../server')
   })
@@ -118,22 +122,27 @@ describe("Route ", () => {
     server.close()
   })
   describe('Get /', ()=> {
-    beforeEach(async () => {
+    beforeEach((done) => {
       try {
-        const data_file = await axios.get("http://localhost:3000/home")
+        var req = {
+          session: {
+            role: 'admin'
+          }
+        }
+        const data_file = axios.get("http://localhost:3000/home", {withCredentials: true})
         .then((response)=> {
           console.log(response)
+          done()
         })
+
       } catch (e) {
         console.error('axios.get failed to execute');
         throw e;  // throwing errors should fail the spec.
       }
     });
-  
-    it("some spec ", async () => {
-      // .getText returns a Promise<string> so you'll need to await it
-      // to get the string value.
+    it('should be expected', (done)=> {
       
-    });
+      done()
+    })
   })
 })
